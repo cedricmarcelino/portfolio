31cd69b795080c5f4be0ce2cb862ac1928fc29b3
Cedric Marcelino <cedricmarcelino1105@gmail.com>
2024-01-22 04:10:06 +0800

    reverted to MUI's documentation on how to apply cache

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,18 @@
 import RootLayout from '@/components/RootLayout/RootLayout';
 import { AppCacheProvider } from '@mui/material-nextjs/v14-pagesRouter';
 import './global.css';
-
 import type { AppProps } from "next/app";
-import ThemeRegistry from '@/utils/ThemeRegistry';
+import { ThemeProvider } from '@emotion/react';
+import theme from '../theme'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <AppCacheProvider>
-      <ThemeRegistry options={{ key: 'mui-theme' }}>
+    <AppCacheProvider {...pageProps}>
+      <ThemeProvider theme={theme}>
         <RootLayout>
           <Component {...pageProps} />
         </RootLayout>
-      </ThemeRegistry>
+      </ThemeProvider>
     </AppCacheProvider>
   );
 }
