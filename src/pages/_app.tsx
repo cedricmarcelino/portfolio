1ec8a4adf59f6cc92fd742dfdf58b16911e79f8c
Cedric Marcelino <cedricmarcelino1105@gmail.com>
2024-01-25 06:44:48 +0800

    removed dynamic title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,28 +7,10 @@ import Head from 'next/head';
 import { ROUTE } from '@/constants/enum';
 
 export default function MyApp({ Component, pageProps, router }: AppProps) {
-  let title;
-  switch(router.pathname) {
-      case ROUTE.HOME:
-        title = 'Home'
-        break;
-      case ROUTE.ABOUT:
-          title = 'About me'
-      break;
-      case ROUTE.CONTACT:
-          title = 'Contact me'
-      break;
-      case ROUTE.PROJECTS:
-          title = 'My Projects'
-      break;
-      default:
-          title = '404'
-          break;
-  }
   return (
     <>
     <Head>
-      <title>{title} | Cedric Jastine Marcelino</title>
+      <title>My Portfolio | Cedric Jastine Marcelino</title>
       <meta name="description" content="A Web Development Portfolio by Cedric Jastine Marcelino" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/favicon.ico" />
