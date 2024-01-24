9dd44333a841b3b4a935ed8958fa2f7f2b0d1d93
Cedric Marcelino <cedricmarcelino1105@gmail.com>
2024-01-25 02:04:09 +0800

    Used CSS Baseline instead of manual reset css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,20 @@
 import RootLayout from '@/components/RootLayout/RootLayout';
 import { AppCacheProvider } from '@mui/material-nextjs/v14-pagesRouter';
-import './global.css';
 import type { AppProps } from "next/app";
 import { ThemeProvider } from '@emotion/react';
 import theme from '../theme'
+import { CssBaseline } from '@mui/material';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <AppCacheProvider {...pageProps}>
-      <ThemeProvider theme={theme}>
-        <RootLayout>
-          <Component {...pageProps} />
-        </RootLayout>
-      </ThemeProvider>
+      <CssBaseline>
+        <ThemeProvider theme={theme}>
+          <RootLayout>
+            <Component {...pageProps} />
+          </RootLayout>
+        </ThemeProvider>
+      </CssBaseline>
     </AppCacheProvider>
   );
 }
